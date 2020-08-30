123d6ae006170c5e81f90df708f3fd20d49b1169
Joacim Granberg <joacim.granberg@outlook.com>
2020-08-30 23:31:12 +0200

    init commit

---
+++ components/Hello.jsx
@@ -0,0 +1,11 @@
+/** @jsx React.DOM */
+'use strict';
+
+var React = require('react');
+class Hello extends React.Component {
+    render() {
+        return (<div>Hello World</div>)
+    }
+}
+
+module.exports = Hello;
