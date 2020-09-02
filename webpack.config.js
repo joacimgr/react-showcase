2fd447d975f3e4df05c9931efefe1c0528640618
Joacim Granberg <joacim.granberg@outlook.com>
2020-09-02 23:11:36 +0200

    fix port

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ module.exports = {
     output: {
         filename: 'bundle.js', //this is the default name, so you can skip it
         //at this directory our bundle file will be available
-        //make sure port 8090 is used when launching webpack-dev-server
-        publicPath: 'http://localhost:8080/assets'
+        //make sure port 9000 is used when launching webpack-dev-server
+        publicPath: 'http://localhost:9000/assets'
     },
     module: {
         loaders: [
