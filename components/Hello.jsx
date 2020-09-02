d7d21026c36b0acaed7a746b6c31d362ae2bd04e
Joacim Granberg <joacim.granberg@outlook.com>
2020-09-02 23:14:10 +0200

    Add content to test

--- components/Hello.jsx
+++ components/Hello.jsx
@@ -6,6 +6,10 @@ class Hello extends React.Component {
     render() {
         return (<div><h1>Hello World</h1>
             <p>I am the greatest Wizard! (of all)</p>
+
+            <div style="height: 200px; margin: 0 auto;">
+                <h1>I am in the middle</h1>
+            </div>
         </div>
                 )
     }
