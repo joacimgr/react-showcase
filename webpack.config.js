b700fd9117a218cd062069bb3f913dfd449a1b8d
Joacim Granberg <joacim.granberg@outlook.com>
2020-09-04 00:49:22 +0200

    asd

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
         filename: 'bundle.js', //this is the default name, so you can skip it
         //at this directory our bundle file will be available
         //make sure port 9000 is used when launching webpack-dev-server
-        publicPath: 'http://localhost:9000/assets'
+        publicPath: 'http://94.46.44.191:8081/assets'
     },
     module: {
         loaders: [
